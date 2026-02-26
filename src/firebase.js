2693ac219be66e8b2d37f525464bb27e99826f05
Maneicel <kimyoon.dev@gmail.com>
2026-02-26 13:16:02 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,3 @@
-// //라스트api 파이어베이스
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-
-// import { getFirestore } from "firebase/firestore";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDGb8cwd4WR8QLYo5dh9WJCu79dmeIi9g0",
-//   authDomain: "lastapi-b673d.firebaseapp.com",
-//   projectId: "lastapi-b673d",
-//   storageBucket: "lastapi-b673d.firebasestorage.app",
-//   messagingSenderId: "772098480670",
-//   appId: "1:772098480670:web:b85b719c8ea23e6ae8ec0f"
-// };
-
-
-//북 리포트 파이어베이스
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
