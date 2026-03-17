ba8af4a4e4413f802bccce917f1561c10e025aa9
axolopole <study.axolo.pole@gmail.com>
2026-03-17 11:04:01 +0900

    firebase 환경변수 적용

---
+++ src/api.js
@@ -0,0 +1,1 @@
+export const BOOK_API_URL = process.env.REACT_APP_BOOK_API_URL;
--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAc0gTM26nm-dGc4jwzkeM66HOnNTpZW-E",
-  authDomain: "bibliorank.firebaseapp.com",
-  projectId: "bibliorank",
-  storageBucket: "bibliorank.firebasestorage.app",
-  messagingSenderId: "525572176935",
-  appId: "1:525572176935:web:5ec497c46dd8ce4b4d6202",
-  measurementId: "G-22KTK1G8YJ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
